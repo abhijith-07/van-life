7e660c432829c7d398c71a3828deb01f88048611
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-28 15:15:47 +0530

    fix: added home page with slideshow images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react"
+import styled, { keyframes } from "styled-components"
+
 export default function Home() {
+    const [showImg, setShowImg] = useState(0);
+
+    const nextImage = () => {
+        setShowImg((prevIndex) => (prevIndex === 3 ? 0 : prevIndex + 1));
+    }
+
+    useEffect(() => {
+        const slideInterval = setInterval(nextImage, 3000);
+        return () => clearInterval(slideInterval);
+      }, [showImg]);
+    
+
     return(
-        <h1>Home, Van Life</h1>
+        <>
+            <section className="hero">
+                <h1>Welcome to Van Life - Your Gateway to Adventure!</h1>
+                <button>Start Exploring</button>
+            </section>
+            <section className="features">
+                <Image src="/images/nature-van.jpg" alt="" {...(showImg === 0) ? {$status:"active"} : {$status:""} }/>
+                <Image src="/images/snow-van.jpg" alt="" {...(showImg === 1) ? {$status:"active"} : {$status:""} }/>
+                <Image src="/images/couple-van.jpg" alt="" {...(showImg === 2) ? {$status:"active"} : {$status:""} }/>
+                <Image src="/images/yellow-van.jpg" alt="" {...(showImg === 3) ? {$status:"active"} : {$status:""} }/>
+            </section>
+        </>
     )
 }
+
+const Image = styled.img`
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+    
+    display: ${props => props.$status === "active" ? "block" : "none" };
+`
