99ebdb81fd59341b482135cac5b8ee57e1720232
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-02 19:03:11 +0530

    fix: fetched van details

--- src/pages/Van.jsx
+++ src/pages/Van.jsx
@@ -3,18 +3,20 @@ import { useParams } from "react-router-dom"
 
 
 export default function Van() {
-    const [vanDetails, setVanDetails] = useState([])
+    const [vanDetails, setVanDetails] = useState({})
     const params = useParams()
 
     useEffect(() => {
         fetch(`/api/van-data/vid/${params.id}`)
         .then(res => res.json())
-        .then(json => console.log(json))
+        .then(json => setVanDetails(json.vans))
     },[])
+
+    console.log(vanDetails)
 
     return(
         <div>
-            {vanDetails}
+            <h1>{vanDetails.name}</h1>
         </div>
     )
 }
