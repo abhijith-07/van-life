44e349fbfd2b715730f1cf156bca5682af59c3d3
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-02 22:38:52 +0530

    fix: styled and updated van detail page

--- src/pages/Van.jsx
+++ src/pages/Van.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
 
 
 export default function Van() {
@@ -15,8 +16,48 @@ export default function Van() {
     console.log(vanDetails)
 
     return(
-        <div>
-            <h1>{vanDetails.name}</h1>
-        </div>
+        <DetailsContainer>
+            <img src={vanDetails.url} alt={vanDetails.name} />
+            <div className="name-price">
+                <h1>{vanDetails.name}</h1>
+                <div className="price">{vanDetails.price}</div>
+            </div>
+            <p>{vanDetails.description}</p>
+        </DetailsContainer>
     )
 }
+
+
+const DetailsContainer = styled.div`
+    width: 100vw;
+
+    img {
+        width: 100%;
+    }
+
+    .name-price {
+        display: flex;
+        justify-content: space-around;
+        height: 2rem;
+        align-items: center;
+        margin-top: 1em;
+    }
+
+    .price {
+        background-color: green;
+        color: #fff;
+        padding: 0.25rem 1rem;
+        border-radius: 5px;
+    }
+
+    p {
+        font-size: 0.75rem;
+        margin-top: 1rem;
+        line-height: 1.25rem;
+        word-spacing: 0.5rem;
+        letter-spacing: 0.15rem;
+        display: flex;
+        padding: 0 1.5rem;
+        text-align: center;
+    }
+`
