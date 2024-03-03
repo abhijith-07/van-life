049ebbf24d04d2a48695b7fd9ebc6d30c48d1f77
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-03 21:53:20 +0530

    fix: react layout added to route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 // Mirage server
 import { makeServer } from "./server"
 import styled from 'styled-components'
+import Layout from './components/Layout'
 
 makeServer()
 
@@ -17,18 +18,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <Navbar>
-            <Link to="/">Home</Link> <br />
-            <Link to="/about">About</Link>
-          </Navbar>
-        </header>
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/vans" element={<VansList />} />
-            <Route path="/vans/van/:id" element={<Van />} />
+            <Route element={ <Layout /> }>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/vans" element={<VansList />} />
+              <Route path="/vans/van/:id" element={<Van />} />
+            </Route>
           </Routes>
         </main>
         <footer>
@@ -44,25 +41,6 @@ function App() {
 
 export default App
 
-
-const Navbar = styled.nav`
-  display: flex;
-  width: 100vw;
-  position: sticky;
-  justify-content: right;
-  align-items: center;
-  gap: 1em;
-  height: 50px;
-  padding: 0.5em 1em;
-  background-color: #190d05;
-  a {
-    text-decoration: none;
-    color: #f6f2f0;
-  }
-  a:hover{
-    color: #cd5302;
-  }
-`
 
 const Contact = styled.div`
   width: 100%;
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+export default function Header() {
+    return(
+        <header>
+          <Navbar>
+            <Link to="/">Home</Link> <br />
+            <Link to="/about">About</Link>
+          </Navbar>
+        </header>
+    )
+}
+
+const Navbar = styled.nav`
+  display: flex;
+  width: 100vw;
+  position: sticky;
+  justify-content: right;
+  align-items: center;
+  gap: 1em;
+  height: 50px;
+  padding: 0.5em 1em;
+  background-color: #190d05;
+  a {
+    text-decoration: none;
+    color: #f6f2f0;
+  }
+  a:hover{
+    color: #cd5302;
+  }
+`
