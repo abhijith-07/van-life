73af12f92fb3f611c9235d241f27c69e8374c5db
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-04 19:37:18 +0530

    fix: Added Link to Dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 export default function Dashboard() {
     return(
         <>
-        Dashboard
+        <Link to="/host">Dashboard</Link>
+        <Link to="/host/income">Income</Link>
+        <Link to="/host/reviews">Reviews</Link>
+        
         <Outlet />
         </>
     )
